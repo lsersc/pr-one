3fb79e125361a2de4af9757cf49608f695e67708
lsersc <126891201+lsersc@users.noreply.github.com>
2023-03-24 12:43:19 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,10 @@
+export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="oned-grass-c060.giwixi7295.workers.dev";  //你需要反代的域名
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+    }
+  };
