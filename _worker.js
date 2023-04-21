1fb31a00a1cf25782034e1dd57af3263391ad854
lsersc <126891201+lsersc@users.noreply.github.com>
2023-04-21 19:45:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="shy-bonus-4059.giwixi7295.workers.dev";  //你需要反代的域名
+        url.hostname="yxlk7-my.sharepoint.com";  //你需要反代的域名
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
